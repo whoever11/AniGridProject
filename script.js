8866f1f76473536c3a7cfcb49e5f867c7ba9c5d4
whoever11 <cjthesedays000@gmail.com>
2024-01-30 10:15:55 -0500

    small changes

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function displayRandomQuestion() {
    if (questionsAndAnswers.length === 0) {
        return
     }
-    const randomQuestion = questionsAndAnswers[Math.floor(Math.random() * questionsAndAnswers.length)]
+    const randomQuestion = questionsAndAnswers[Math.floor(Math.random() * questionsAndAnswers.length)]//random question from array of questions and answers
     questionObj = randomQuestion // Assign randomQuestion to questionObj
     document.querySelector("#question").textContent = questionObj.question
 }
@@ -41,21 +41,21 @@ let score = 0
 document.getElementById(`submit`).addEventListener(`click`, function() {
     let userAnswer = document.getElementById(`answer`).value
     if (userAnswer.toLowerCase() === questionObj.answer.toLowerCase()) {
-        alert(`Correct!`)
+        alert(`Correct!`)//alerts if answer is correct and changes score
         score += 1
     } else {
-        alert(`Incorrect!`)
+        alert(`Incorrect!`)//alerts if answer is incorrect and changes score
         score -= 1
     } 
-document.getElementById(`answer`).value = ''
-document.getElementById('score').innerText = score.toString()
+document.getElementById(`answer`).value = ''//clears the answer box after submitting it 
+document.getElementById('score').innerText = score.toString()//converts score to string
     
 })
 
-// restart button
+// restart button and everything on the screen
 document.getElementById(`restart`).addEventListener(`click`, function() {
-    document.getElementById(`answer`).value = ``
-    document.getElementById(`question`).innerText = ``
-    document.getElementById(`score`).innerText = ``
+    document.getElementById(`answer`).value = ``//clears the answer box
+    document.getElementById(`question`).innerText = ``//clears the question box
+    document.getElementById(`score`).innerText = ``//clears the score box
 })
 
